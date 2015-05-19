97a07b1a9f121e41c3da4c165b2e6ef4624f1597
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-19 18:34:55 +0100

    Test 8 done

--- questions_spec.rb
+++ questions_spec.rb
@@ -40,7 +40,7 @@ describe 'Ruby Homework' do
     expect(n).to eq [4, 5, 6, 7]
   end
 
-  it 'add_element_to_beginning_of_array' do
+  fit 'add_element_to_beginning_of_array' do
     n = add_element_to_beginning_of_array [2, 3, 4, 5], 1
     expect(n).to eq [1, 2, 3, 4, 5]
   end
